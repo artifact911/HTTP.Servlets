63573a11173ca8c8a3268a2cb796a3a21b8e4439
artifact911 <artifact911@gmail.com>
2022-09-06 17:04:29 +0300

    lesson 9 done

--- src/main/java/org/example/socket/SocketRunner.java
+++ src/main/java/org/example/socket/SocketRunner.java
@@ -4,26 +4,40 @@ import lombok.SneakyThrows;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketRunner {
 
     @SneakyThrows
     public static void main(String[] args) {
+        var inetAddress = Inet4Address.getByName("localhost");
+        try (var socket = new Socket(inetAddress, 7777);
+             var outputStream = new DataOutputStream(socket.getOutputStream());
+             var inputStream = new DataInputStream(socket.getInputStream());
+             var scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                var request = scanner.nextLine();
+                outputStream.writeUTF(request);
+                System.out.println("Response from server: " + inputStream.readUTF());
+            }
+        }
+    }
+
+    private static void singleResponse() throws IOException {
         // http - 80
         // https - 443
         // мы тут юзаем порт tcp
+        var inetAddress = Inet4Address.getByName("localhost");
 
-        var inetAddress = Inet4Address.getByName("google.com");
-
-        try (var socket = new Socket(inetAddress, 80);
+        try (var socket = new Socket(inetAddress, 7777);
              var outputStream = new DataOutputStream(socket.getOutputStream());
              var inputStream = new DataInputStream(socket.getInputStream())) {
 
             outputStream.writeUTF("Hello World!");
-            var response = inputStream.readAllBytes();
-            System.out.println(response.length);
+            System.out.println("Response from server: " + inputStream.readUTF());
         }
     }
 }
