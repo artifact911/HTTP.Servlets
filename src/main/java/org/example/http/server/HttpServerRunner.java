913e824b8289bb9dd8188fc5e857c5110a1720bf
artifact911 <artifact911@gmail.com>
2022-09-07 22:41:50 +0300

    lesson18 done

---
+++ src/main/java/org/example/http/server/HttpServerRunner.java
@@ -0,0 +1,9 @@
+package org.example.http.server;
+
+public class HttpServerRunner {
+
+    public static void main(String[] args) {
+        var httpServer = new HttpServer(9000);
+        httpServer.run();
+    }
+}
